3e1bcbf11e1ab09f66be2904b13ca49611bec00a
Лезовский Станислав <lezsv@reso.ru>
2024-05-06 09:49:48 +0300

    123

--- Sources/Models/FileStruct.swift
+++ Sources/Models/FileStruct.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public struct FileData: Codable {
     
-    let fileData: Data?
+    public let fileData: Data?
     
     enum CodingKeys: String, CodingKey {
         case fileData = "FILEDATA"
